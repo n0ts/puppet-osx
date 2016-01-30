6b54ca2a4b68eca6b500bd5e6c7242a7d7848861
n0ts <me@n0ts.org>
2016-01-31 00:06:07 +0900

    Fix power nap condition

--- manifests/energy/enable_power_nap.pp
+++ manifests/energy/enable_power_nap.pp
@@ -3,6 +3,6 @@ class osx::energy::enable_power_nap {
   exec { 'Enable Power Nap':
       command => 'pmset -c darkwakes 1',
       user    => root,
-      unless  => 'pmset -g custom | grep \'AC Power\' -A 17 | grep -E \'^\sdarkwakes\s+1\'',
+      unless  => 'pmset -g custom | grep \'AC Power\' -A 17 | grep -E \'^\spowernap\s+1\'',
   }
 }
